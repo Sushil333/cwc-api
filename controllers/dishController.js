fc8b764e0784e374698cdc82337d650b431aefdd
Sushil333 <Sushilbhardwaj705@gmail.com>
2021-11-21 09:46:05 +0000

    update

--- controllers/dishController.js
+++ controllers/dishController.js
@@ -3,7 +3,6 @@ import asyncHandler from 'express-async-handler';
 import Dish from '../models/dish.js';
 import Store from '../models/store.js';
 
-
 /**
  * @desc    create dish
  * @route   POST /api/store/dishes/create
@@ -12,7 +11,7 @@ import Store from '../models/store.js';
 export const createDish = asyncHandler(async (req, res) => {
   const imgUrl =
     'https://lh3.googleusercontent.com/EGD6C34eosVtKkElJybCOXIaOC-_iw5lYXzOmGdfMpjtaqijt1Oym_ltWjs5uyx3d8jo0ew8rg7tzFVMWszKGSbL3Ny_=w256';
-  const { name, description, price } = req.body;
+  const { dishName, description, price } = req.body;
 
   const hasStore = await Store.findOne({ owner: req.user.id });
   if (!hasStore) {
@@ -20,7 +19,7 @@ export const createDish = asyncHandler(async (req, res) => {
   }
 
   let errorList = [];
-  if (!name) errorList.push('name is required');
+  if (!dishName) errorList.push('dishName is required');
   if (!description) errorList.push('description is required');
   if (!price) errorList.push('price is required');
 
@@ -28,7 +27,7 @@ export const createDish = asyncHandler(async (req, res) => {
     res.status(400).json({ message: errorList });
   } else {
     const dish = new Dish({
-      name,
+      dishName,
       description,
       price,
       storeId: hasStore.id,
